81d2808d886bb9ed48cc9a1475c7c55090b393a3
mikepenzin <mike.penzin@gmail.com>
2017-04-26 19:10:21 +0000

    Added form forwarding

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var compression             = require('compression'),
-    express                 = require("express"),
+var express                 = require("express"),
+    bodyParser              = require("body-parser"),
+    compression             = require('compression'),
+    nodemailer              = require('nodemailer'),
     app                     = express();
 
 app.use(compression(9));
 
+var smtpTransport = nodemailer.createTransport('SMTP', {
+    service: 'SendGrid',
+    auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+      }
+});
+
 // to not use .ejs ending
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public", { maxAge: 8640000000 }));
+
+//tell express to use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 //General Routes
 app.get("/", function(req, res){
@@ -19,6 +32,28 @@ app.get("/projects", function(req, res){
 
 app.get("/contact", function(req, res){
     res.render("contact");
+});
+
+app.post("/contact", function(req,res){
+   console.log(req.body.name);
+   var contact = {
+       name:req.body.name,
+       email:req.body.email,
+       message:req.body.message
+   };
+   var mailOptions = {
+            to: 'mike.penzin@gmail.com',
+            from: contact.email,
+            subject: 'Personal Portfolio',
+            text: 'Hello,\n\n' +
+              'This is email from' + contact.name + '(' + contact.email + ')' + '.\n' +
+              contact.message
+          };
+    smtpTransport.sendMail(mailOptions, function(err) {
+        console.log("Message sent successfully!");
+        res.render("contact");
+    });
+        
 });
 
 app.get("*", function(req, res){
